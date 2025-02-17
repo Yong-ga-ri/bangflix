c7d4f8d9052bc2c354b04a881f6c2d9e798a161e
Yong-ga-ri <rlfgks97@gmail.com>
2025-02-17 16:42:09 +0900

    refactor: reviewService 메서드 순서 재구성

--- backend/src/main/java/com/swcamp9th/bangflixbackend/domain/review/service/ReviewService.java
+++ backend/src/main/java/com/swcamp9th/bangflixbackend/domain/review/service/ReviewService.java
@@ -14,25 +14,20 @@ import org.springframework.web.multipart.MultipartFile;
 
 public interface ReviewService {
 
-    void createReview(CreateReviewDTO newReview, List<MultipartFile> images, Member member);
-
-    void deleteReview(ReviewCodeDTO reviewCodeDTO, int memberCode);
-
     List<ReviewDTO> findReviewsWithFilters(Integer themeCode, String filter, Pageable pageable);
     List<ReviewDTO> findReviewsWithFilters(Integer themeCode, String filter, Pageable pageable, int memberCode);
-
-    void likeReview(ReviewCodeDTO reviewCodeDTO, int memberCode);
-
-    void deleteLikeReview(ReviewCodeDTO reviewCodeDTO, int memberCode);
-
     StatisticsReviewDTO findReviewStatistics(Integer themeCode);
 
     List<ReviewDTO> getReviewDTOS(List<Review> sublist);
     List<ReviewDTO> getReviewDTOS(List<Review> sublist, int memberCode);
 
     ReviewDTO getReviewDTO(Review review, Integer memberCode);
-
     ReviewReportDTO findReviewReport(int memberCode);
-
     List<ReviewDTO> findReviewByMemberCode(int memberCode, Pageable pageable);
+
+    void likeReview(ReviewCodeDTO reviewCodeDTO, int memberCode);
+    void deleteLikeReview(ReviewCodeDTO reviewCodeDTO, int memberCode);
+
+    void createReview(CreateReviewDTO newReview, List<MultipartFile> images, Member member);
+    void deleteReview(ReviewCodeDTO reviewCodeDTO, int memberCode);
 }
